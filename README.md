04ff5c3a42cf0aaca6523cccba66469ca070df6a
ChenZhen <imlex@live.cn>
2017-12-27 14:45:45 +0800

    improve
    Koudai48 fix; add group.py

--- README.md
+++ README.md
@@ -14,6 +14,9 @@
 ##  更新记录
 
 
+**2017.12.27更新**：口袋48数据格式更新，代码简化；增加了主文件`group.py`，可以对群内消息进行管理操作；优化了`setting.py`的结构，更具可观性。
+
+
 **2017.12.16更新**：v2.0发布。模块化重构了整个项目，使之更简洁；根据摩点（微打赏）官方API重构了摩点（微打赏）查询，更加简洁高效；增加手动获取口袋48登录信息（token），token存活期为30天。
 
 ## 文件介绍
@@ -23,6 +26,9 @@
 
 
 `koudai48.py` 启动文件2，包含口袋48查询的定时任务
+
+
+`group.py` 启动文件3，包含QQ群扩展功能
 
 
 `setting.py`  所有的参数设置
@@ -43,11 +49,11 @@
   
   2.运行一次qqbot：终端输入`qqbot`，运行，然后退出。
   
-  3.[插件配置](https://github.com/pandolia/qqbot#插件的配置-pluginpath-和-plugins-)：将`qqbot_hzx/`下6个文件拷贝至` ~/.qqbot-tmp/plugins`
+  3.[插件配置](https://github.com/pandolia/qqbot#插件的配置-pluginpath-和-plugins-)：将`qqbot_hzx/`下7个文件拷贝至` ~/.qqbot-tmp/plugins`
 
   4.修改`setting.py`中所有参数
   
-  5.[修改配置文件](https://github.com/pandolia/qqbot#配置文件的使用方法)，将`start.py`以及`koudai48.py`添加到需要加载的插件列表。在终端输入`qqbot -u somebody`来运行
+  5.[修改配置文件](https://github.com/pandolia/qqbot#配置文件的使用方法)，将`start.py`以及`koudai48.py`以及`group.py`添加到需要加载的插件列表。在终端输入`qqbot -u somebody`来运行
   
   6.详细信息请阅读[qqbot说明文档](https://github.com/pandolia/qqbot/blob/master/README.MD)
 
@@ -77,5 +83,40 @@
     - 设置方法可参考[nikochan.cc](http://www.nikochan.cc/2017/08/03/Crawlerweibonotloggin/)
 
 
+`gettoken.py`：
 
 
+  * 手动获取token
+
+
+`koudai48.py`：
+
+
+  * 查询制定成员口袋房间，返回新消息
+  * 直播提醒
+
+
+`modian.py`：
+
+
+  * 摩点相关查询
+    - 项目每笔集资
+    - 金额榜／打卡榜
+    - 总览
+
+
+`weibo.py`：
+
+
+  * 返回微博相关信息
+    - 新微博（原创／转发）（带图／不带图）
+
+
+`group.py`：
+
+
+  * 群操作
+    - 新成员欢迎
+    - 被at操作
+    - 关键词禁言
+    - 查撤回
--- setting.py
+++ setting.py
@@ -1,28 +1,37 @@
 # -*- coding: utf-8 -*-
 
 
-# qq群号
-def groupid():
-    id = 'QQ群号填这里'
-    return id
+# 小偶像名字
+def idol_name():
+    idol_name = "黄子璇"
+    return idol_name
+
+
+# ----------------------摩点微打赏设置----------------------
 
 
 # 微打赏名称
 def wds_name():
-    wds_name = "项目名称填在这里"
+    wds_name = "摩点（微打赏）项目未设置\n拿小樱花的测试一下"
     return wds_name
 
 
 # 微打赏网址 建议使用短地址t.cn
 def wds_url():
-    wds_url = "摩点（微打赏）地址填这里"
+    wds_url = "摩点（微打赏）地址未设置"
     return wds_url
 
 
 # 微打赏项目对应pro_id
 def pro_id():
-    pro_id = 10285
+    pro_id = 10250
     return pro_id
+
+
+# --------------------------------------------------------
+
+
+# ----------------------口袋48设置----------------------
 
 
 # 口袋48:memberId
@@ -39,10 +48,62 @@ def roomId():
     return roomId
 
 
-# 口袋48:小偶像名字
-def idol_name():
-    idol_name = "黄子璇"
-    return idol_name
+# 设置口袋48账号密码
+def user(type):
+    user = '口袋48用户名'
+    password = '密码'
+    if type == 1:
+        return user
+    elif type == 2:
+        return password
+
+
+# 手动运行gettoken.py手动获取token，然后填入下方
+# token 存活时间为 30 天
+def token():
+    token = 'token填在这里'
+    return token
+
+
+# --------------------------------------------------------
+
+
+# ----------------------qq群设置----------------------
+
+
+# qq群号
+def groupid():
+    id = '606642799'
+    return id
+
+
+# 欢迎信息
+def welcome():
+    msg = "欢迎聚聚加入BEJ48-黄子璇的应援群！" + '\n' + "大家可以叫子璇宝宝叁玖 ~" + '\n' +\
+        "叁玖是一只土生土长的北京姑娘，生日是5月20日，生在2002年的金牛座。" + '\n' + "九春三秋，只为遇见你。" +\
+        '\n' + "希望你我可以相知相识～" + '\n' + '\n' + "三分钟带你领略甜甜的叁玖" + '\n' +\
+        "首演unit《爱的魔法》：http://t.cn/RolMikW" + '\n' + '\n' +\
+        "出道以来第五场公演unit《爱的魔法》：http://t.cn/RCb8QBn" + '\n' + '\n' +\
+        "第四届总决选拉票公演：http://t.cn/RCb8gzH" + '\n' + '\n' +\
+        "更多补档内容请戳b站：BEJ48-黄子璇应援会 http://t.cn/RCbRLjZ" + '\n' + '\n' +\
+        "最后也请聚聚关注下叁玖的微博与超级话题吧" + '\n' + "@BEJ48-黄子璇：http://t.cn/RCbRiAe" +\
+        '\n' + "@BEJ48-黄子璇应援会：http://t.cn/RCbRNu1" + '\n' +\
+        "#黄子璇#超级话题：http://t.cn/RCbRQg2" + '\n' + '\n' +\
+        "让我们陪着这个刚出道不久的小孩子长大，看着她成为更好的人，一起给她最好的应援吧~（鞠躬）"
+    return msg
+
+
+# 关键词触发
+# 禁言关键词,留空则无禁言
+def shutup():
+    wordlist = []
+    return wordlist
+
+
+# --------------------------------------------------------
+
+
+# ----------------------微博设置----------------------
 
 
 # 手机网页版微博地址
@@ -57,18 +118,4 @@ def weibo_id():
     return id
 
 
-# 设置口袋48账号密码
-def user(type):
-    user = '口袋48用户名'
-    password = '密码'
-    if type == 1:
-        return user
-    elif type == 2:
-        return password
-
-
-# 手动运行gettoken.py手动获取token，然后填入下方
-# token 存活时间为 30 天
-def token():
-    token = 'token填这里'
-    return token
+# --------------------------------------------------------
